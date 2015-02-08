b1805444fd8a5a15c7319344f4f480b91a1c4171
Shelby Kelly <shelby.kelly@colorado.edu>
2015-02-08 15:57:17 -0700

    fixed hash

--- person.rb
+++ person.rb
@@ -1,13 +1,13 @@
 KATY_PERRY = {
-  "first_name" => "Katy",
-  "last_name" => "Perry",
-  "superstar_status" => true,
-  "address" => {
-    "street" => "123 Hollywood Boulevard",
-    "city" => "Los Angeles",
-    "state" => "CA",
-    "zip_code" => 90210
+  :first_name => "Katy",
+  :last_name => "Perry",
+  :superstar_status => true,
+  :address => {
+    :street => "123 Hollywood Boulevard",
+    :city => "Los Angeles",
+    :state => "CA",
+    :zip_code => 90210
   },
-  "favorite_foods" => ["sushi", "hamburgers", "pho"],
-  "cats_name" => "kitty purry"
+  :favorite_foods => ["sushi", "hamburgers", "pho"],
+  :cats_name => "kitty purry"
 }
